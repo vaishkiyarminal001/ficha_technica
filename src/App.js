38624c4ddecb2b9e728777e3e59dccc6a48e0629
vaishkiyarminal001 <vaishkiyarminal001@gmail.com>
2023-10-06 16:13:19 +0530

    Created paages for the website

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Gallery from './Component/Pages/Gallery';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <Gallery/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Pages/Gallery.jsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react"
+import "./Gallery.css";
+import PersonIcon from '@mui/icons-material/Person';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import ColorLensIcon from '@mui/icons-material/ColorLens';
+
+export default function Gallery() {
+
+    const [state, setState] = useState([]);
+    console.log(state);
+
+
+    useEffect(() =>{
+        DisplayPaint();
+    },[]);
+
+    let url = `http://localhost:8080/paint`;
+
+    const DisplayPaint = async() =>{
+        const res = await fetch(url);
+        const data = await res.json();
+        setState(data);
+        console.log(data);
+    } 
+
+
+  return (
+    <div className="conatiner">
+        <h1 className="heading">Art</h1>
+
+
+  {/* mapping */}
+<div className="display-card">
+        {
+           state.map((e) =>(
+            <div key={e.id} className="card">
+                <img src={e.image} alt={e.title} width="450px" height="450px"/>
+                {/* <h1>{e.id}</h1> */}
+
+                <div className="details">
+                    <h2><PersonIcon className="icon"/>Nombre: {e.name}</h2>
+                    <hr/>
+
+                    <div className="detail2">
+                    <h3><ColorLensIcon className="icon"/>Técnica:{e.technique}</h3>
+                    <h3><ColorLensIcon className="icon"/>Técnica:{e.technique}</h3>
+                    <h4><CalendarMonthIcon className="icon"/>{e.dateofpaint}</h4>
+                    <h4><CalendarMonthIcon className="icon"/>{e.dateofpaint}</h4>
+                   </div>
+
+                </div>
+
+                <button className="showmore">Show More</button>
+            </div>
+           ))
+        }
+
+
+</div>
+
+    </div>
+  )
+}
